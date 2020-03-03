dd8d2bd408b42f350eee99e5994f4b9d54e13203
shoukugeki02 <59277996+shoukugeki02@users.noreply.github.com>
2020-03-03 17:29:48 +0700

    Update lab16_2.cpp

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 //Write the definition of myString() here
+char myString(char *x,int y){
+char *R = new char ;
+for(int i=0;i<y;i++)
+{
+	R[i]='A'+i;
+	cout << R[i];
+}
 
+R[y] = '\0';
+}
 int main(){
 	char *p;
 	int N;	
